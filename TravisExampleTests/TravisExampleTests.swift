79c8e1b920c87406b0dda340f5926db35cacb7ad
Belinda Carrera <belinda.carrera@vass.es>
2018-11-21 12:18:06 +0100

    ejemplo en el test para producir el error

--- TravisExampleTests/TravisExampleTests.swift
+++ TravisExampleTests/TravisExampleTests.swift
@@ -24,6 +24,8 @@ class TravisExampleTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let failure = (1+1) == 3
+        XCTAssertTrue(failure, "1 + 1 = 2")
 
     }
     
